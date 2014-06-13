35b63de5388379cc815cc201b3c3274b15057952
guptag <guptagaruda@gmail.com>
2014-06-13 17:27:13 -0400

    apply layout

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 var HistoricalPrices = require('./data/historicalprices'),
 	CandleStickChart = require('./controls/candlestickchart'),
+	LayoutEngine =  require('./layout/layoutengine'),
 	Q = require('q'),
 	$ = require('jquery');
+
+
+function init() {
+	LayoutEngine.addLayout("topbarlayout", function(w, h) {
+		return {
+			width: w,
+			height: 40,
+			top: 0,
+			left: 0
+		};
+	});
+
+	LayoutEngine.addLayout("plotlayout", function(w, h) {
+		return {
+			width: w,
+			height: h - 40,
+			top: 40,
+			left: 0
+		};
+	});
+}
+
+
+function bindControls() {
+  $("#ticker").keypress(function(event){
+		var keyCode = (event.keyCode ? event.keyCode : event.which);
+		if(keyCode == 13){
+			renderChart(this.value);
+		}
+	});
+}
 
 
 function renderChart (ticker) {
@@ -26,18 +58,12 @@ function renderChart (ticker) {
 	})
 }
 
-function bindControls() {
-  $("#ticker").keypress(function(event){
-		var keyCode = (event.keyCode ? event.keyCode : event.which);
-		if(keyCode == 13){
-			renderChart(this.value);
-		}
-	});
-}
+init();
 
 $( document ).ready(function() {
+   LayoutEngine.applyLayouts();
    bindControls();
-	 renderChart($("#ticker").attr("data-default"));
+   renderChart($("#ticker").attr("data-default"));
 });
 
 
