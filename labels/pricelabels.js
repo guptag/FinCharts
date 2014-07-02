f97b65a1a1b41f851d88efeb929e6c1f2b9ffb07
guptag <guptagaruda@gmail.com>
2014-07-02 18:01:58 -0400

    stub code for labels

---
+++ labels/pricelabels.js
@@ -0,0 +1,11 @@
+var _ = require('lodash');
+
+
+var increments = [0.01, 0.05, 0.1, 0.2, 0.25, 0.5, 1, 2, 5, 10, 25, 50, 100,
+                  200, 500, 1000, 2000, 5000, 10000, 50000, 100000, 500000];
+
+var PriceLabels = {
+    generate = function (minValue, maxValue, total) {
+
+    }
+}
