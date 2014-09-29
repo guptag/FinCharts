64093c3ff17dafe02e606b098727411f0440b434
guptag <guptagaruda@gmail.com>
2014-09-28 21:17:19 -0400

    early version to seperate client and server modules

---
+++ app/index.js
@@ -0,0 +1,7 @@
+var app = require("./js/app.js");
+var $ = require('jquery');
+
+$(document).ready(function () {
+    console.log("app init 123");
+    app.init();
+});
---
+++ app/js/ui/components/temp.js
@@ -0,0 +1,11 @@
+/** @jsx React.DOM */
+
+var React = require("react");
+
+var HelloMessage = React.createClass({
+  render: function() {
+    return <div>Hello {this.props.name}</div>;
+  }
+});
+
+module.exports = HelloMessage;
