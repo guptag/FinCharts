8696c2ce31539ea7544c4d4f588f5cdc785d95c2
guptag <guptagaruda@gmail.com>
2014-11-28 14:30:44 -0500

    functionality for the atomstoremanager

--- app/client/ui/core/atomCommand.js
+++
@@ -1,8 +0,0 @@
-
-var AtomCommand = function (commandName, commandData) {
-    this.name = commandName;
-    this.data = commandData;
-    this.timestamp = new Date().getTime();
-};
-
-module.exports = AtomCommand;
--- app/client/ui/core/atomstoremanager.js
+++ app/client/ui/core/atomstoremanager.js
@@ -1,4 +1,54 @@
 
+var _ = window.server.lodash;
+
+var AtomCommand = require(".atomcommand");
+var AppStore = require("./stores/appstore");
+var ChartStore = require("./stores/chartstore");
+var LayoutStore = require("./stores/layoutstore");
+var WatchlistStore = require("./stores/watchliststore");
+
+function AtomStoreManager(atom) {
+
+    (function init() {
+        this.stores = {
+            appStore: new AppStore(atom),
+            chartStore: new ChartStore(atom),
+            layoutStore: new LayoutStore(atom),
+            watchlistStore: new WatchlistStore(atom)
+        }
+    })();
+
+    this.prototype = {
+        handleCommands: function (commands) {
+            _.each(commands, function (command) {
+                this.handleCommand(command);
+            }, this)
+        },
+
+        handleCommand: function (command) {
+            if (typeof command !== AtomCommand) {
+                throw new Error("AtomStoreManager: command isn't AtomCommand", command);
+            }
+
+            var storeName = command.store,
+                action = command.action,
+                params = command.commandData;
+
+            if (!this.stores[storeName]) {
+                throw new Error("AtomStoreManager: cannot handle command. invalid store name", arguments);
+            }
+
+            if (!this.stores[storeName][method]) {
+                throw new Error("AtomStoreManager: invalid action", arguments);
+            }
+
+            // call the action method
+            this.stores[storeName][method].call(this.stores[storeName], params)
+        }
+    }
+
+}
+
 
 
 
