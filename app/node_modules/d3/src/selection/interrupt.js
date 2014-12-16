1e18c1f953c0f8cc8bcab3e5518f27b031930ca2
guptag <guptagaruda@gmail.com>
2014-12-16 15:12:18 -0500

    Add d3

---
+++ app/node_modules/d3/src/selection/interrupt.js
@@ -0,0 +1,26 @@
+// import "../transition/transition";
+import "selection";
+
+// TODO Interrupt transitions for all namespaces?
+d3_selectionPrototype.interrupt = function(name) {
+  return this.each(name == null
+      ? d3_selection_interrupt
+      : d3_selection_interruptNS(d3_transitionNamespace(name)));
+};
+
+var d3_selection_interrupt = d3_selection_interruptNS(d3_transitionNamespace());
+
+function d3_selection_interruptNS(ns) {
+  return function() {
+    var lock, active;
+    if ((lock = this[ns]) && (active = lock[lock.active])) {
+      if (--lock.count) {
+        delete lock[lock.active];
+        lock.active += .5;
+      } else {
+        delete this[ns];
+      }
+      active.event && active.event.interrupt.call(this, this.__data__, active.index);
+    }
+  };
+}
