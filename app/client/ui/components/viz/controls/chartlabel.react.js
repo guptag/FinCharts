d4ec072c1bf99edd6254b7645fe1cb3a41def016
guptag <guptagaruda@gmail.com>
2014-12-30 15:24:06 -0700

    render chart labels

--- app/client/ui/components/viz/controls/chartlabel.react.js
+++ app/client/ui/components/viz/controls/chartlabel.react.js
@@ -1,14 +1,27 @@
 /** @jsx React.DOM */
 
 var React = require("react/addons");
+var moment = require("moment");
+var AppContext = require("ui/core/appcontext");
 
 var ChartInfo = React.createClass({
     render: function() {
+        var chartStore = AppContext.stores.chartStore;
+        var ticker = chartStore.getTicker();
+        var duration = chartStore.getDuration();
+        var timeframe = chartStore.getTimeframe();
+
+        var timeframeStr =  moment(timeframe.from).format("ll") +
+                                " - " +
+                                moment(timeframe.to).format("ll")
+
+        var durationStr = "(" + duration + ")";
+
         return (
             <g className="chart-info" transform="translate(10, 30)">
-                <text x="0" y="0" className="ticker">MSFT</text>
-                <text x="0" y="20" className="range">1 Jan 2014 - 10 Oct 2014</text>
-                <text x="160" y="20" className="period">(weekly)</text>
+                <text x="0" y="0" className="ticker">{ticker}</text>
+                <text x="0" y="20" className="range">{timeframeStr}</text>
+                <text x="180" y="20" className="period">{durationStr}</text>
             </g>
         );
     }
