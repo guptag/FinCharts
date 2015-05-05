9a73081599ccd448789b8d065bdc63396bba9460
guptag <guptagaruda@gmail.com>
2015-05-05 10:11:47 -0700

    add deferred events module

---
+++ app/client/ui/core/events/deferredevents.js
@@ -0,0 +1,29 @@
+var _ = require("lodash");
+var q = require("q");
+
+function DeferredEvents() {
+    this.events = {};
+}
+
+DeferredEvents.prototype.register = function (id, cb) {
+    if (!this.events[id]) {
+      this.events[id] = Q.defer();
+    }
+
+    this.events[id].resolve(cb);
+};
+
+DeferredEvents.prototype.trigger = function (id, payload) {
+  if (!this.events[id]) {
+    this.events[id] = Q.defer();
+  }
+
+  this.events[id].promise.fcall(payload);
+}
+
+DeferredEvents.prototype.clear = function (id) {
+  this.events[id] = null;
+}
+
+// singleton
+module.exports = new DeferredEvents();
