6118caf388e3f330beec8f301a64f398056d5ad4
guptag <guptagaruda@gmail.com>
2022-12-30 16:53:48 -0800

    fix yahoo data

--- README.md
+++ README.md
@@ -1,67 +1,66 @@
-Stock Charts (Electron.js) desktop application using React and Immutable.js
-------
+## Stock Charts (Electron.js) desktop application using React and Immutable.js
 
 ![screen shot 2015-07-03 at 6 41 24 pm](https://cloud.githubusercontent.com/assets/1994225/8506015/6451129a-21b3-11e5-8f2a-4fd266274be6.png)
 
 ![screen shot 2015-07-03 at 6 42 16 pm](https://cloud.githubusercontent.com/assets/1994225/8506016/657f1f7c-21b3-11e5-8a81-691a781f6a07.png)
 
-Perf metrics
-------------
+## Perf metrics
+
 ![screen shot 2015-02-13 at 4 44 03 am](https://cloud.githubusercontent.com/assets/1994225/6187428/4deb3094-b33b-11e4-9867-3248ae81917d.png)
 
-Architecture (draft)
---------------------
+## Architecture (draft)
 
-* Application leverages [React's Flux](http://fluxxor.com/what-is-flux.html) architecture where the data/state changes flow in an uni-directional way making the application easy to reason about.
-* Describes the entire state of the application in an immutable persistent data structure (using immutable.js) similar to OM's style of global atom state. Any user action creates an action message which updates the app state creating a new state reference and triggers a re-render of the app. Due to the immutable nature of the state, React components can leverage "canComponentUpdate" just by comparing the state references. This speeds up the rendering (by skipping the render step when not needed) on top of virtual dom comparision provided by React.
-* Q promises for coordinating async tasks.
-* Downloaded ticker data will be stored on disk (for now).
+- Application leverages [React's Flux](http://fluxxor.com/what-is-flux.html) architecture where the data/state changes flow in an uni-directional way making the application easy to reason about.
+- Describes the entire state of the application in an immutable persistent data structure (using immutable.js) similar to OM's style of global atom state. Any user action creates an action message which updates the app state creating a new state reference and triggers a re-render of the app. Due to the immutable nature of the state, React components can leverage "canComponentUpdate" just by comparing the state references. This speeds up the rendering (by skipping the render step when not needed) on top of virtual dom comparision provided by React.
+- Q promises for coordinating async tasks.
+- Downloaded ticker data will be stored on disk (for now).
 
-Run
----
+## Run
+
 (from root)
- * nvm use (current v6.6.0)
- * npm install
- * gulp (builds and opens the app)
- * gulp dev (builds, adds file watchers and opens the app)
 
-Build
------
-* [x] gulp
-* [x] stylus
-* [x] nib
-* [x] node-webkit-builder
-* [x] watchers (dev)
-* [x] auto reload (dev)
+- nvm use (current v6.6.0)
+- npm install
+- gulp (builds and opens the app)
+- gulp dev (builds, adds file watchers and opens the app)
+- gulp package-app (packages app in target\package)
 
+## Build
 
-Chart Components
------------------------
-* [x] Axis, Labels
-* [x] Y-axis (Prices, Percentages)
-* [ ] Scales
- * [x] Normal
- * [ ] Log
-* [x] Grid Lines
-* [x] Main chart (line, area, candlestick, OHL, OHLC)
-* [x] Volume bars
-* [x] Crosshairs
-* [ ] Split view (compare ticker, RSI, ADX etc)
-* [x] Multi chart layout (3H, 3V, 2H, 2V)
-* [x] Animate chart
-* [ ] Listing of added modules on the chart (with remove option)
+- [x] gulp
+- [x] stylus
+- [x] nib
+- [x] node-webkit-builder
+- [x] watchers (dev)
+- [x] auto reload (dev)
 
-Settings
----------------
-* [x] Timeframe (3m, 6m, 1y, 3y, 5y, all, custom)
-* [x] Daily, Weekly, Monthly
-* [ ] Sync crosshairs
-* [ ] Compare tickers
-* [ ] Technical indicators
-    * [ ] Moving Averages
-    * [ ] RSI
-    * [ ] Bollingar Bands
-    * [ ] ADX
-    * [ ] Avg Volume
-    * [ ] Fib ranges
-* [ ] Themes (light/dark)
+## Chart Components
+
+- [x] Axis, Labels
+- [x] Y-axis (Prices, Percentages)
+- [ ] Scales
+- [x] Normal
+- [ ] Log
+- [x] Grid Lines
+- [x] Main chart (line, area, candlestick, OHL, OHLC)
+- [x] Volume bars
+- [x] Crosshairs
+- [ ] Split view (compare ticker, RSI, ADX etc)
+- [x] Multi chart layout (3H, 3V, 2H, 2V)
+- [x] Animate chart
+- [ ] Listing of added modules on the chart (with remove option)
+
+## Settings
+
+- [x] Timeframe (3m, 6m, 1y, 3y, 5y, all, custom)
+- [x] Daily, Weekly, Monthly
+- [ ] Sync crosshairs
+- [ ] Compare tickers
+- [ ] Technical indicators
+  - [ ] Moving Averages
+  - [ ] RSI
+  - [ ] Bollingar Bands
+  - [ ] ADX
+  - [ ] Avg Volume
+  - [ ] Fib ranges
+- [ ] Themes (light/dark)
--- app/client/data/api/pricedata.js
+++ app/client/data/api/pricedata.js
@@ -1,132 +1,152 @@
 var request = window.server.request,
-    Q = require("q"),
-    moment = require('moment'),
-    _ = require("lodash"),
-    sprintf = require("sprintf-js").sprintf,
-    db = require("data/db/finchartsdb");
+  Q = require("q"),
+  moment = require("moment"),
+  _ = require("lodash"),
+  sprintf = require("sprintf-js").sprintf,
+  db = require("data/db/finchartsdb");
 
 var PriceDataApi = {
-    /**
-     *  chartKeys: {
-     *      ticker: "MSFT",
-     *      timeframe: {
-     *          from: <Date>,
-     *          to: <Date>
-     *      },
-     *      duration: 'daily'
-     *  }
-     */
-    getTickerDataAsync: function (chartKeys) {
-        return getCookieCrumb()
-                 .then(function (crumb) {
-                    return fetchData(crumb, chartKeys);
-                  })
-                 .then(parseData);
-    }
+  /**
+   *  chartKeys: {
+   *      ticker: "MSFT",
+   *      timeframe: {
+   *          from: <Date>,
+   *          to: <Date>
+   *      },
+   *      duration: 'daily'
+   *  }
+   */
+  getTickerDataAsync: function (chartKeys) {
+    return getCookieCrumb()
+      .then(function (crumb) {
+        return fetchData(crumb, chartKeys);
+      })
+      .then(parseData);
+  },
 };
 
 var yahooSessionCache;
 
 function getCookieCrumb() {
-     if (yahooSessionCache) {
-        return Q.resolve(yahooSessionCache);
-     } else {
-       var re = /CrumbStore\":{\"crumb\":\"(.*?)\"}/gi;
-       return getResponseFromServer("https://finance.yahoo.com/quote/MSFT/history")
-                  .then((data) => {
-                    var matches = re.exec(data.body);
+  if (yahooSessionCache) {
+    return Q.resolve(yahooSessionCache);
+  } else {
+    var re = /CrumbStore\":{\"crumb\":\"(.*?)\"}/gi;
+    return getResponseFromServer(
+      "https://finance.yahoo.com/quote/MSFT/history"
+    ).then((data) => {
+      var matches = re.exec(data.body);
 
-                    // cache session data
-                    yahooSessionCache = {
-                      cookie: data.response.headers['set-cookie'][0] || "",
-                      crumb: matches[1] || ""
-                    };
+      // cache session data
+      yahooSessionCache = {
+        cookie: "",
+        crumb: "",
+      };
 
-                    // clear the cache after an hour
-                    setTimeout(function () {
-                      yahooSessionCache = null;
-                    }, 1000 * 60 * 60);
+      // clear the cache after an hour
+      setTimeout(function () {
+        yahooSessionCache = null;
+      }, 1000 * 60 * 60);
 
-                    return yahooSessionCache;
-                  });
-      }
-   }
+      return yahooSessionCache;
+    });
+  }
+}
 
 function fetchData(cookieData, chartKeys) {
-    var fetchDeferred = Q.defer(), ticker = chartKeys.ticker;
+  var fetchDeferred = Q.defer(),
+    ticker = chartKeys.ticker;
 
-    var toDate = chartKeys.timeframe.to;
-    var fromDate = chartKeys.timeframe.from;
-    // var toDate = moment().toDate();
-    // var fromDate = moment(toDate).subtract(chartKeys.timeframe.from, 'months').toDate();
-    var duration = chartKeys.duration === "daily" ? "d" : (chartKeys.duration === "weekly" ? "w" : "m");
+  var toDate = chartKeys.timeframe.to;
+  var fromDate = chartKeys.timeframe.from;
+  // var toDate = moment().toDate();
+  // var fromDate = moment(toDate).subtract(chartKeys.timeframe.from, 'months').toDate();
+  var duration =
+    chartKeys.duration === "daily"
+      ? "d"
+      : chartKeys.duration === "weekly"
+      ? "w"
+      : "m";
 
-    var interval = (function (period) {
-          switch (period) {
-            case "daily": return "1d";
-            case "weekly": return "1wk";
-            case "monthly": return "1mo";
-            default: return "1d";
-          }
-        })(chartKeys.duration);
+  var interval = (function (period) {
+    switch (period) {
+      case "daily":
+        return "1d";
+      case "weekly":
+        return "1wk";
+      case "monthly":
+        return "1mo";
+      default:
+        return "1d";
+    }
+  })(chartKeys.duration);
 
-    //https://query1.finance.yahoo.com/v7/finance/download/MSFT?period1=1466191294&period2=1497727294&interval=1mo&events=history&crumb=wQMXQHsHwRC
+  //https://query1.finance.yahoo.com/v7/finance/download/MSFT?period1=1466191294&period2=1497727294&interval=1mo&events=history&crumb=wQMXQHsHwRC
 
-    var dataUrl = sprintf("https://query1.finance.yahoo.com/v7/finance/download/%s?period1=%s&period2=%s&interval=%s&events=history&crumb=%s",
-                                        ticker,
-                                        Math.floor((fromDate.getTime()) / 1000),
-                                        Math.floor((toDate.getTime()) / 1000),
-                                        interval,
-                                        cookieData.crumb);
+  var dataUrl = sprintf(
+    "https://query1.finance.yahoo.com/v7/finance/download/%s?period1=%s&period2=%s&interval=%s&events=history&crumb=%s",
+    ticker,
+    Math.floor(fromDate.getTime() / 1000),
+    Math.floor(toDate.getTime() / 1000),
+    interval,
+    cookieData.crumb
+  );
 
+  var cacheKey = sprintf(
+    "%s_%s_%s_%s",
+    ticker.toLowerCase(),
+    duration,
+    moment(toDate).format("YYYYMMDD"),
+    moment(fromDate).format("YYYYMMDD")
+  );
 
-    var cacheKey = sprintf("%s_%s_%s_%s", ticker.toLowerCase(), duration, moment(toDate).format('YYYYMMDD'), moment(fromDate).format('YYYYMMDD'));
-
-    getFromCache(cacheKey)
-      .then(function (rowData /* {key: "", data: ""} */) {
-        if (rowData) {
-          fetchDeferred.resolve({priceData: rowData.data, ticker: ticker});
-        } else {
-          getDataFromServer(dataUrl, { cookie: cookieData.cookie })
-            .then(function (priceData) {
-              addToCache(cacheKey, priceData)
-                .then(function () {
-                  fetchDeferred.resolve({priceData: priceData, ticker: ticker});
-                });
-            });
-        }
+  getFromCache(cacheKey).then(function (rowData /* {key: "", data: ""} */) {
+    if (rowData) {
+      fetchDeferred.resolve({ priceData: rowData.data, ticker: ticker });
+    } else {
+      getDataFromServer(dataUrl, { cookie: cookieData.cookie }).then(function (
+        priceData
+      ) {
+        addToCache(cacheKey, priceData).then(function () {
+          fetchDeferred.resolve({ priceData: priceData, ticker: ticker });
+        });
       });
+    }
+  });
 
-    return fetchDeferred.promise;
+  return fetchDeferred.promise;
 }
 
 function getFromCache(cacheKey) {
   return db.pricetable
-          .where('key')
-          .equals(cacheKey)
-          .first()
-          .then(function (priceData) {
-            console.log("data loaded from indexDb", priceData);
-            return priceData;
-          }).catch(function (error) {
-            console.log("IndexDb error:", error);
-            return "";
-          });
+    .where("key")
+    .equals(cacheKey)
+    .first()
+    .then(function (priceData) {
+      console.log("data loaded from indexDb", priceData);
+      return priceData;
+    })
+    .catch(function (error) {
+      console.log("IndexDb error:", error);
+      return "";
+    });
 }
-
 
 function getDataFromServer(dataUrl, headers) {
   console.log("loading data from", dataUrl, headers);
   var serverDataDeferred = Q.defer();
-  request({
-    url: dataUrl,
-    headers: headers
-    }, function (error, response, body) {
+  request(
+    {
+      url: dataUrl,
+      headers: headers,
+    },
+    function (error, response, body) {
       if (!error && response.statusCode === 200) {
         console.log("data downloaded");
         serverDataDeferred.resolve(body);
       }
-    });
+    }
+  );
   return serverDataDeferred.promise;
 }
 
@@ -138,7 +158,7 @@ function getResponseFromServer(dataUrl) {
       console.log("data downloaded");
       serverDataDeferred.resolve({
         response: response,
-        body: body
+        body: body,
       });
     }
   });
@@ -148,11 +168,11 @@ function getResponseFromServer(dataUrl) {
 function addToCache(cacheKey, priceData) {
   console.log("adding data to indexdb", cacheKey);
   return db.pricetable
-          .add({key: cacheKey, data: priceData})
-          .catch(function (error) {
-            console.log("IndexDb add error:", error, cacheKey);
-            return "";
-          });
+    .add({ key: cacheKey, data: priceData })
+    .catch(function (error) {
+      console.log("IndexDb add error:", error, cacheKey);
+      return "";
+    });
 }
 
 // Jan12 - Jan7 - daily - http://finance.yahoo.com/q/hp?s=YHOO&a=00&b=12&c=1996&d=00&e=7&f=2014&g=d
@@ -163,70 +183,72 @@ function addToCache(cacheKey, priceData) {
 // data - {priceData: "string", ticker: "ticker"}
 
 function parseData(data) {
-    var returnData = {
-        status: 'loaded',
-        series: [],
-        min: Infinity,
-        max: -Infinity,
-        minVolume: Infinity,
-        maxVolume: -Infinity
+  var returnData = {
+    status: "loaded",
+    series: [],
+    min: Infinity,
+    max: -Infinity,
+    minVolume: Infinity,
+    maxVolume: -Infinity,
+  };
+
+  var parseDeferred = Q.defer();
+
+  var dateRecords = data.priceData.split("\n");
+  // console.log(dateRecords);
+
+  _.each(dateRecords, function (dataRow) {
+    var record = (dataRow || "").split(",");
+
+    if (
+      record.length !== 7 ||
+      isNaN(record[1]) ||
+      isNaN(record[2]) ||
+      isNaN(record[3]) ||
+      isNaN(record[4]) ||
+      isNaN(record[5]) ||
+      isNaN(record[6])
+    ) {
+      return;
+    }
+
+    var data = {
+      date: Date.parse(record[0]),
+      open: +parseFloat(record[1]).toFixed(2),
+      high: +parseFloat(record[2]).toFixed(2),
+      low: +parseFloat(record[3]).toFixed(2),
+      close: +parseFloat(record[4]).toFixed(2),
+      volume: +parseFloat(record[6]).toFixed(2),
+      adjClose: +parseFloat(record[5]).toFixed(2),
     };
 
-    var parseDeferred = Q.defer();
+    // Patch the data issues
+    if (data.close < data.low) {
+      data.close = data.adjClose;
+    }
 
-    var dateRecords = data.priceData.split('\n');
-    // console.log(dateRecords);
-
-    _.each(dateRecords, function (dataRow) {
-        var record = (dataRow || "").split(',');
-
-        if (record.length !== 7 || isNaN(record[1]) || isNaN(record[2]) ||
-            isNaN(record[3]) || isNaN(record[4]) || isNaN(record[5]) ||
-            isNaN(record[6])) {
-            return;
-          }
-
-        var data = {
-            date: Date.parse(record[0]),
-            open: +parseFloat(record[1]).toFixed(2),
-            high: +parseFloat(record[2]).toFixed(2),
-            low: +parseFloat(record[3]).toFixed(2),
-            close: +parseFloat(record[4]).toFixed(2),
-            volume: +parseFloat(record[6]).toFixed(2),
-            adjClose: +parseFloat(record[5]).toFixed(2)
-        };
-
-        // Patch the data issues
-        if (data.close < data.low) {
-          data.close = data.adjClose;
-        }
-
-        // adjust prices for splits (unfortunately yahoo includes dividends in adjClose)
-        // the prices displayed in this tool will be off by a bit
-        /*var adjRatio = data.adjClose / data.close;
+    // adjust prices for splits (unfortunately yahoo includes dividends in adjClose)
+    // the prices displayed in this tool will be off by a bit
+    /*var adjRatio = data.adjClose / data.close;
         data.open = formatNumber(data.open * adjRatio);
         data.high = formatNumber(data.high * adjRatio);
         data.low = formatNumber(data.low * adjRatio);
         data.close = formatNumber(data.close * adjRatio);*/
 
-        returnData.series.push(data);
+    returnData.series.push(data);
 
-        if (data.low < returnData.min)
-            returnData.min = data.low;
+    if (data.low < returnData.min) returnData.min = data.low;
 
-        if (data.high > returnData.max)
-            returnData.max = data.high;
+    if (data.high > returnData.max) returnData.max = data.high;
 
-        if (data.volume < returnData.minVolume)
-            returnData.minVolume = data.volume;
+    if (data.volume < returnData.minVolume) returnData.minVolume = data.volume;
 
-        if (data.volume > returnData.maxVolume)
-            returnData.maxVolume = data.volume;
-      });
+    if (data.volume > returnData.maxVolume) returnData.maxVolume = data.volume;
+  });
 
-      parseDeferred.resolve(returnData);
+  parseDeferred.resolve(returnData);
 
-      return parseDeferred.promise;
+  return parseDeferred.promise;
 }
 
 /*function formatNumber(number) {
